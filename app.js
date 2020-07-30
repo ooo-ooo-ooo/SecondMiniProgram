6e5e56d9f75c5037925749e6b9002e6c15048d26
ltt <245343723@qq.com>
2020-07-30 10:04:25 +0800

    12_

--- app.js
+++ app.js
@@ -1,39 +1,61 @@
 //app.js
 App({
+
+  /* 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）*/
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
+    const token = wx.getStorageSync('token');
+    if(token && token.length !== 0){
+      this.check_token(token)
+    }else{
+      this.login()
+    }    
+  },
+  check_token(token){
+    console.log('执行了验证token的操作')
+    wx.request({
+      url: 'http://123.207.32.32:3000/auth',
+      header: {
+        token
+      },
+      method: 'post',
       success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
+        if(!res.data.errCode){
+          this.globalData.token = token;
+        }else{
+          this.login()
         }
+      },
+      fail: err => {
+        console.log(err)
       }
     })
   },
+
+  login(){
+    console.log('执行了登录操作')
+    wx.login({
+      success: res => {
+        const code = res.code;
+        wx.request({
+          url: 'http://123.207.32.32:3000/login',
+          data: {
+            code
+          },
+          method: 'post',
+          success: res => {
+            const token = res.data.token;
+            this.globalData.token = token;
+            wx.setStorageSync('token', token)
+          },
+          fail: err => {
+            console.log(err)
+          }
+        })
+      },
+    })
+  },
+
   globalData: {
-    userInfo: null
+    token: ''
   }
 })
